9db18346ef5f24345e831859bce6fb4dcf179732
Afsalshanavs2 <Afsalshanavas02@gmail.com>
2026-01-01 16:20:15 +0530

    Updated second Views.py

--- app/views.py
+++ app/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 from .models import Students
 
-# Create your views here.
-
-def home(request):
-    return render(request, 'app/home.html')
-
 def student_list(request):
     students = Students.objects.all()
     return render(request, 'app/student_list.html', {'students': students})
